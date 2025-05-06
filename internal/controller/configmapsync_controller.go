1e03b07b38ffafe0752ff31d27dde31251256a58
Bryant <andersonciputra@gmail.com>
2025-05-06 13:27:01 +1000

    fix: annotation

--- internal/controller/configmapsync_controller.go
+++ internal/controller/configmapsync_controller.go
@@ -40,6 +40,7 @@ type ConfigMapSyncReconciler struct {
 // +kubebuilder:rbac:groups=apps.bryantanderson.github.io,resources=configmapsyncs,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps.bryantanderson.github.io,resources=configmapsyncs/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=apps.bryantanderson.github.io,resources=configmapsyncs/finalizers,verbs=update
+// +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
